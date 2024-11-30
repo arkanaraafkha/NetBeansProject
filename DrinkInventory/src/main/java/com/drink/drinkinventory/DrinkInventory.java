0ccb7b365db30a225970e870af701423f2058d8a
arkanaraafkha <arkan.araafkha11@gmail.com>
2024-11-30 23:52:17 +0700

    Initial commit for Praktikum RPL2

---
+++ DrinkInventory/src/main/java/com/drink/drinkinventory/DrinkInventory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.drink.drinkinventory;
+
+/**
+ *
+ * @author ACER
+ */
+public class DrinkInventory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ WarehouseManagementSystem/src/main/java/com/warehouse/warehousemanagementsystem/WarehouseManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.warehouse.warehousemanagementsystem;
+
+/**
+ *
+ * @author ACER
+ */
+public class WarehouseManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
